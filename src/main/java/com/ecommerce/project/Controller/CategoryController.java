49babae5f296299983f58a477320815d8565bf5e
Prathamdiwakar <prathamdiwakar44@gmail.com>
2026-02-02 13:35:14 +0530

    Making Categrory controller for Application

---
+++ src/main/java/com/ecommerce/project/Controller/CategoryController.java
@@ -0,0 +1,40 @@
+package com.ecommerce.project.Controller;
+
+import com.ecommerce.project.Model.Category;
+import com.ecommerce.project.service.CategoryServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+public class CategoryController {
+    @Autowired
+    private CategoryServices categoryServices;
+
+
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories() {
+        return categoryServices.getAllcategories();
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category) {
+        categoryServices.createCategory(category);
+        return "Category created Successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
+        try{
+            String status = categoryServices.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        }catch (ResponseStatusException e){
+            return new ResponseEntity<>(e.getReason(),e.getStatusCode());
+        }
+    }
+}
