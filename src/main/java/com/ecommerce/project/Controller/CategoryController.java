27e52e1f9d7349f4fbe60cc524a9918abda8b20e
Prathamdiwakar <prathamdiwakar44@gmail.com>
2026-02-03 19:52:55 +0530

    Adding UpdateCategory To Ecom-Website

--- src/main/java/com/ecommerce/project/Controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/Controller/CategoryController.java
@@ -37,4 +37,16 @@ public class CategoryController {
             return new ResponseEntity<>(e.getReason(),e.getStatusCode());
         }
     }
+
+    @PutMapping("/admin/categories/{categoryId}")
+    public ResponseEntity<String> updateCategory(@RequestBody Category category,
+                                                 @PathVariable Long categoryId) {
+        try {
+            Category savedCategory = categoryServices.updateCategory(category,categoryId);
+             return new ResponseEntity<>("UpdatedCategory With CategoryId: "+categoryId, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+
+    }
 }
