0635fb8ef63410fff711c7b60d18feb18d903e9a
Prathamdiwakar <prathamdiwakar44@gmail.com>
2026-02-22 20:03:44 +0530

    Adding More api's to are Product Module

--- src/main/java/com/ecommerce/project/Controller/ProductController.java
+++ src/main/java/com/ecommerce/project/Controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.ecommerce.project.Controller;
 
-import com.ecommerce.project.Model.Product;
 import com.ecommerce.project.payload.ProductDTO;
 import com.ecommerce.project.payload.ProductResponse;
 import com.ecommerce.project.service.ProductServices;
@@ -8,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
@@ -16,10 +16,10 @@ public class ProductController {
     ProductServices productServices;
 
     @PostMapping("/admin/categoryies/{categoryId}/product")
-    public ResponseEntity<ProductDTO> addProduct(@RequestBody Product  product,
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO  productDTO,
                                                  @PathVariable Long categoryId) {
-    ProductDTO productDTO = productServices.addProduct(categoryId,product);
-    return new ResponseEntity<>(productDTO, HttpStatus.CREATED);
+    ProductDTO savedProductDTO = productServices.addProduct(categoryId,productDTO);
+    return new ResponseEntity<>(savedProductDTO, HttpStatus.CREATED);
     }
 
     @GetMapping("/public/products")
@@ -39,4 +39,25 @@ public class ProductController {
         ProductResponse productResponse = productServices.getProductByKeyword(keyword);
         return new ResponseEntity<>(productResponse,HttpStatus.OK);
     }
+
+    @PutMapping("/admin/products/{productId}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long productId,
+                                                         @RequestBody ProductDTO productDTO) {
+      ProductDTO savedProductDTO = productServices.updateProductByProductId(productId, productDTO);
+      return new ResponseEntity<>( savedProductDTO, HttpStatus.FOUND);
+    }
+
+    @DeleteMapping("/admin/products/{productId}")
+    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId){
+        ProductDTO productDTO = productServices.deleteProductById(productId);
+        return new ResponseEntity<>(productDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("product/{productId}/image")
+    public ResponseEntity<ProductDTO> updateProductImage(@PathVariable Long productId,
+                                                         @RequestParam("image") MultipartFile image) {
+        ProductDTO productDTO = productServices.UpdateProductImage(productId,image);
+        return new ResponseEntity<>(productDTO, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/ecommerce/project/service/ProductServices.java
+++ src/main/java/com/ecommerce/project/service/ProductServices.java
@@ -3,13 +3,20 @@ package com.ecommerce.project.service;
 import com.ecommerce.project.Model.Product;
 import com.ecommerce.project.payload.ProductDTO;
 import com.ecommerce.project.payload.ProductResponse;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ProductServices {
-    ProductDTO addProduct(Long categoryId, Product product);
+    ProductDTO addProduct(Long categoryId, ProductDTO productDTO);
 
     ProductResponse getAllProduct();
 
     ProductResponse getProductbyId(Long categoryId);
 
     ProductResponse getProductByKeyword(String keyword);
+
+    ProductDTO updateProductByProductId(Long productId, ProductDTO productDTO);
+
+    ProductDTO deleteProductById(Long productId);
+
+    ProductDTO UpdateProductImage(Long productId, MultipartFile image);
 }
