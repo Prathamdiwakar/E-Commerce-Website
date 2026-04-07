c07b4084b9d40b9f4e54223e741e8681c5825eb1
Prathamdiwakar <prathamdiwakar44@gmail.com>
2026-04-07 07:05:43 +0530

    Implementing Payment Class to the application

---
+++ src/main/java/com/ecommerce/project/repositories/PaymentRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.project.repositories;
+
+import com.ecommerce.project.Model.Order;
+import com.ecommerce.project.Model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment,Long> {
+}
